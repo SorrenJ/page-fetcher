4e26c68a0a4a81a43d70a2ebc3f7559079115f0d
SorrenJ <soalexjao@gmail.com>
2024-04-26 20:09:35 -0700

    error handling and console log for sucessful

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,29 @@
 
 let args = process.argv.slice(2);
-let destination = "/home/labber/lighthouse/page-fetcher/fetchedData.txt";
+let destination = "/home/labber/lighthouse/page-fetcher/fetchedHTML.txt";
 
 const fs = require('fs');
 
 const request = require('request');
-request('http://www.google.com', (error, response, body) => {
+request('http://www.example.edu/ ', (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
+
+
+fs.writeFile(destination, body, err => {
+
+  if (err) {
+    console.error(err);
+  }
+
+  // "file written successfully" message, we're assuming 1 character = 1 byte
+  console.log(`Downloaded and saved ${body.length} bytes to ${destination}.`);
 });
+
+});
+
+
 
 
 /*
